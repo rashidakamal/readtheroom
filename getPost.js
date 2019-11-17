164f0d1567a3bbf30d24aca00187ccc48dd4f9a6
Rashida Kamal <rsk2161@columbia.edu>
2019-11-17 13:05:23 -0500

    better

--- getPost.js
+++ getPost.js
@@ -29,31 +29,48 @@ function serverStart() {
   console.log('Server listening on port '+ port);
 }
 
-// this is called by both GET and POST handlers,
-// to format a response to the request:
-function formatResponse(thisContent) {
-	var result = 'I will say:' + '\n' + thisContent.name;
-  	return result;
-}
-
-
 function newCandidate(request, response){
 
-	console.log("in newCandidate");
-	var defaultList = [5]; 
+	console.log("I got a GET request");
+	var candidateName = request.params.name;
+
+	// console.log("in newCandidate");
+	var defaultList = []; 
+	
 	allCandidates.push(defaultList); // adding a default value to the end of our candidates array
 	var candidateID = allCandidates.length - 1;
+	console.log(allCandidates);
 
-	// RK Idea: if we add a "name" parameter, then we can parrot back the added name to the client. 
-
-	var content = 'You added a new candidate to the sentiment meter. Their candidate ID is: ' + candidateID + '\n';
-	response.end(content);
+	var content = "You have added " + candidateName + " as a candidate. Their ID is " + candidateID + ".\n";
+	response.send(content);
+	response.end();
 
 }
 
 function candidateRating(request, response){
-	// current average of all submitted votes
 
+	var candidateID = request.params.num;
+
+	var currentCandidateRatings = allCandidates[candidateID]; // this is a list! 
+
+	var sumRatings = []; 
+
+		for (i=0; i < currentCandidateRatings.length; i++) {
+
+			var iRating = currentCandidateRatings[i].rating; 
+			sumRatings.push(iRating);
+			console.log(iRating); 
+
+		}
+	var averageRating = sumRatings.reduce((a,b) => a + b, 0) / currentCandidateRatings.length; 
+
+	console.log("Current average rating for candidate: " + candidateID + " is ");
+	console.log(currentCandidateRatings); 
+	console.log(averageRating); 
+
+	var content = "printing average rating to log";
+	response.send(content);
+	response.end();
 
 }
 
@@ -61,30 +78,48 @@ function candidateRating(request, response){
 function vote(request, response) {
 
 	var candidateID = request.params.num;
-	var newRating = request.params.vote; 
+	var newRating = parseInt(request.params.vote); 
 
-	// allCandidates[candidateID] = newRating; // replaces value at this index; 
-	console.log(request.headers);
+		// need to implement some logic to ensure that ratings are not below 0 or above 5
+
+	currentCandidateRatings = allCandidates[candidateID]; 
+
+	var newVote = {time: Date.now(), rating: newRating};
+	currentCandidateRatings.push(newVote);
+
+	// edit this so we're returning json, not messages. 
+	var content = "trying to vote damnnit";
+	response.send(content);
+	response.end();
 
 }
 
 function candidateHistory(request, response) {
-	// return json with timestamps & ratings
+
+	// Dear Andrew and Jackie,
+	// If you'd like, you can maybe parse our vote objects for ratings and timestamps, and then provide an average of rating for a given duration of time
+	// Ya know, some sort of candidate history...
+	// Chart?
+	// Json?
+
+	// much love,
+	// Anna, Rashida, & SJ
+
 }
 
+function defaultContent(request, response){
+
+	var content = "This is the default content.";
+	response.send(content);
+	response.end();
+
+}
 // start the server:
 server.listen(8080, serverStart);
 
-// we can use the request headers to uniquely track each voter 
-// but also do we want that kind of life? 
-// so that they can submit votes simply by hitting up /candidate/:num/:vote
+server.get('/default', defaultContent); // returns candidate ID #
 
-server.get('/candidate/new/', newCandidate); // returns candidate ID #
-server.get('/candidate/:num/:', candidateRating); // returns current rating of candidate
-server.get('/candidate/:num/:vote', vote); // submits new rating/vote, 1-5 "stars" or "points" or w/e
-server.get('/candidate/:num/history', candidateHistory); 
-
-// // we can use the request headers to uniquely track each voter... 
-// server.get('/voter/new/', newVoter); // adds a new number, returns voter ID #
-// server.get('/voter/:num/', voterRating); // get current voter rating
-// server.get('/voter/:num/history', voterHistory); // returns voter rating history
+server.get('/candidate/new/:name', newCandidate); // returns candidate ID #
+server.get('/candidate/:num/', candidateRating); // returns current rating of candidate
+server.get('/candidate/:num/vote/:vote', vote); // submits new rating/vote, 1-5 "stars" or "points" or w/e
+// server.get('/candidate/:num/history', candidateHistory); 
